08e967cf8adc6be2c84bf256abba6500ad27044e
James Arnott <james@rno.tt>
2025-05-30 20:44:20 +0100

    fix: deployment + title

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GAN-i3-365-decoder/', // Add base path for GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
