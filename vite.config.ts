683d7528b06c41e411ffc941533e776a9acb8264
James Arnott <james@rno.tt>
2025-06-09 20:55:11 +0100

    fix: pages + added info in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/GAN-i3-365-decoder/', // Add base path for GitHub Pages
+  base: '/gan-i360-bluetooth/', // Add base path for GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
